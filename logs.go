fa1395c1c5a08c113cee7cc3c015043412ceda6c
mjason <tywf91@gmail.com>
2013-05-02 18:34:10 +0800

    添加日志系统

---
+++ logs.go
@@ -0,0 +1,33 @@
+package ngw
+
+import (
+	"fmt"
+	seelog "github.com/cihub/seelog"
+)
+
+var Logger seelog.LoggerInterface
+
+func loadAppConfig() {
+	logger, err := seelog.LoggerFromConfigAsFile("./seelog.xml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	UseLogger(logger)
+}
+
+func init() {
+	DisableLog()
+	loadAppConfig()
+}
+
+// DisableLog disables all library log output
+func DisableLog() {
+	Logger = seelog.Disabled
+}
+
+// UseLogger uses a specified seelog.LoggerInterface to output library log.
+// Use this func if you are using Seelog logging system in your app.
+func UseLogger(newLogger seelog.LoggerInterface) {
+	Logger = newLogger
+}
